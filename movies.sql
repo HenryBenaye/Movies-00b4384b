e619ed008a562836dad2891c1798cfd9dd6337a4
HENRY BENAYE <2106747@talnet.nl>
2020-03-18 15:30:09 +0100

    Uitwerking van de exercise

---
+++ movies.sql
@@ -0,0 +1,30 @@
+-- create table Movies (name varchar(20));
+
+-- alter table Movies
+-- add column id int NOT NULL auto_increment primary key;
+
+-- alter table Movies
+-- add column titel varchar(50);
+
+-- alter table Movies
+-- add column duur time;
+
+-- alter table Movies
+-- add column datum_van_uitkomst date;
+-- alter table Movies
+-- add column land_van_uitkomst varchar(50);
+
+-- alter table Movies
+-- add column omschrijving varchar(50);
+
+-- alter table Movies
+-- add column trailer varchar(50);
+
+
+insert into  movies (titel, duur, datum_van_uitkomst, land_van_uitkomst, omschrijving)
+values("12 Angry Men ", '01:36:00','1957-04-10', "USA","A jury holdout attempts to prevent a miscarriage of justice by forcing his colleagues to reconsider the evidence.")
+
+
+
+
+
